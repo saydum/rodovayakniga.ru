88efef1e50fe02399f1b0586399a41d01a8b9884
Saydum <saydum.it5@gmail.com>
2024-07-03 11:42:52 +0300

    feat: create CRUD Human
    implemented CRUD controller  Human

--- app/Http/Controllers/HumanController.php
+++ app/Http/Controllers/HumanController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Human;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\Human\CreateHumenRequest;
+use App\Http\Requests\Human\UpdateHumanRequest;
 
 class HumanController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $humans = Human::paginate(10);
+        return view('humans.index', compact('humans'));
+    }
+
+    public function create(): View
+    {
+        return view('humans.create');
+    }
+
+    public function store(CreateHumenRequest $request): RedirectResponse
+    {
+        Human::create($request->validated());
+        return redirect()->route('humans.index')->with('success', 'Human has been created.');
+    }
+
+    public function show(Human $human): View
+    {
+        return view('humans.show', compact('human'));
+    }
+
+    public function edit(Human $human): View
+    {
+        return view('humans.edit', compact('human'));
+    }
+
+    public function update(UpdateHumanRequest $request): RedirectResponse
+    {
+        Human::update($request->validated());
+        return redirect()->route('humans.index')->with('status', 'Human successfully updated.');
+    }
+
+    public function destroy(Human $human): RedirectResponse
+    {
+        $human->delete();
+        return redirect()->route('humans.index')->with('success', 'Human successfully deleted.');
+    }
 }
