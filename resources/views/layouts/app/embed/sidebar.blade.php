59efc65e038d93249e98113f25c0758e72bef348
Saydum <saydum.it5@gmail.com>
2024-07-09 23:54:14 +0300

    chore: update icon home

--- resources/views/layouts/app/embed/sidebar.blade.php
+++ resources/views/layouts/app/embed/sidebar.blade.php
@@ -1,2 +1,2 @@
-<x-nav-item title="Главное" icon="box" route="home"/>
+<x-nav-item title="Главное" icon="house-fill" route="home"/>
 <x-nav-item title="Родня" icon="people-fill" route="humans.index"/>
