f22ddde8237d48b960d8e309a3514304f222fce0
Saydum <saydum.it5@gmail.com>
2024-07-11 16:30:46 +0300

    chore: rename item menu human

--- resources/views/layouts/app/embed/sidebar.blade.php
+++ resources/views/layouts/app/embed/sidebar.blade.php
@@ -1,2 +1,2 @@
 <x-nav-item title="Главное" icon="house-fill" route="home"/>
-<x-nav-item title="Родня" icon="people-fill" route="humans.index"/>
+<x-nav-item title="Родственники" icon="people-fill" route="humans.index"/>
