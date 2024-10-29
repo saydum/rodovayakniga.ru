314a68dbbae0721f5d06b279b2a49d03db9ccbd8
saydum <saydum.it5@gmail.com>
2024-10-29 18:10:06 +0300

    fix: rename application to app

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('App.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    @vite(['resources/sass/application.scss', 'resources/js/application.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
 <div id="app">
